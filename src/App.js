c90400d8e64af7728fba854c7c5d9c5bbc99dc3f
TanPL <polytan7@1utar.my>
2023-07-10 04:07:16 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import addImg from "./addImg.png";
 
 function App() {
+  const [file, setFile] = useState(localStorage.getItem("uploadedImage"));
+  const [activeTab, setActiveTab] = useState("input");
+
+  function handleChange(e) {
+    const selectedFile = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const image = reader.result;
+      setFile(image);
+      localStorage.setItem("uploadedImage", image);
+    };
+
+    reader.readAsDataURL(selectedFile);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col-6 left">
+        <div className="tab">
+          <div
+            className={`nav-link ${activeTab === "input" ? "active" : ""}`}
+            onClick={() => setActiveTab("input")}
+          >
+            Image Gallery
+          </div>
+
+          <div
+            className={`nav-link ${activeTab === "preview" ? "active" : ""}`}
+            onClick={() => setActiveTab("preview")}
+          >
+            Preview
+          </div>
+        </div>
+        <div className="tab-content d-flex flex-fill">
+          {activeTab === "input" ? (
+            <div className="w-100 h-100 d-flex align-items-center justify-content-center">
+              <label htmlFor="imageUpload" className="image-upload">
+                <img className="img-center" src={addImg} />
+
+                <input
+                  type="file"
+                  id="imageUpload"
+                  onChange={handleChange}
+                  style={{ display: "none" }}
+                />
+              </label>
+            </div>
+          ) : (
+            <div className="w-100 h-100 d-flex align-items-center justify-content-center">
+              {file && (
+                <img width="100%" height="100%" src={file} alt="Preview" />
+              )}
+              {!file && <p>No image selected.</p>}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="col-6"></div>
     </div>
   );
 }
